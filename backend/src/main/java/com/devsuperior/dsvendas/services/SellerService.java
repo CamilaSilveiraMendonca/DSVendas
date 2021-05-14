3929af5f099bfdd3eb5bb7117ab96e8513c7458d
Camila Silveira <mila-camila2008@hotmail.com>
2021-05-14 17:55:54 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll(); //resultado da busca de dados
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList()); //converte cada item do result para SellerDTO e depois transforma tudo em uma lista
+	}
+}
