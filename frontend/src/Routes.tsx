e6ca847653c67e72d3451cba4efe4a31deee6755
Camila Silveira <mila-camila2008@hotmail.com>
2021-07-04 12:49:25 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -9,7 +9,7 @@ const Routes = () => {
                 <Route path="/" exact>
                     <Home />
                 </Route>
-                <Route path="/">
+                <Route path="/dashboard">
                     <Dashboard />
                 </Route>
             </Switch>
